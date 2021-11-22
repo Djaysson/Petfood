a29b5c40caee72dc150bfdfc3002ca8ee4f2678b
Djaysson <djayson.dev@gmail.com>
2021-11-22 17:38:23 -0300

    Feat(cadastro): add user registration page

--- web/src/pages/cadastro/index.jsx
+++ web/src/pages/cadastro/index.jsx
@@ -1,4 +1,56 @@
+import Header from "../../components/header/index";
+import Illustration from "../../assets/illustration-1.svg";
+
 const Cadastro = () => {
-  return <h1>Cadastro</h1>;
+  return (
+    <div className="container-fluid  h-100 bg-primary ">
+      <Header darkVersion />
+      <div className="row align-items-center text-center  ">
+        <div className="col-6 text-right my-auto">
+          <img
+            src={Illustration}
+            className="img-fluid img"
+            alt="Person with pet"
+          />
+        </div>
+        <div className="col-6">
+          <div className="box col-8">
+            <h2 className="text-center mb-4">
+              Falta pouco pra fazer o seu pet feliz.
+            </h2>
+            <input
+              type="text"
+              className="form-control form-control-lg mb-3"
+              placeholder="Nome completo"
+            />
+            <input
+              type="text"
+              className="form-control form-control-lg mb-3"
+              placeholder="E-mail"
+            />
+            <input
+              type="text"
+              className="form-control form-control-lg mb-3"
+              placeholder="Telefone"
+            />
+            <input
+              type="text"
+              className="form-control form-control-lg mb-3"
+              placeholder="CPF"
+            />
+            <input
+              type="date"
+              className="form-control form-control-lg mb-3"
+              placeholder="Data de Nascimento"
+            />
+
+            <button className="btn btn-lg btn-block btn-secondary">
+              Finalizar Pedido
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Cadastro;
