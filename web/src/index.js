6f751d844ad152fc8b86cd188281308e7280ba94
Djaysson <djayson.dev@gmail.com>
2021-11-22 17:52:11 -0300

    Refactor: add modifications to project files

--- web/src/index.js
+++ web/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./styles/global.css";
 import Cadastro from "./pages/cadastro/index";
 
 ReactDOM.render(
