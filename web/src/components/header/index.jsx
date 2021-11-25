1fa7f3f8badc2ba460864180fa369cf46371897f
Djaysson <djayson.dev@gmail.com>
2021-11-25 15:05:07 -0300

    Feat (checkout): add the user's purchase data checkout page

--- web/src/components/header/index.jsx
+++ web/src/components/header/index.jsx
@@ -4,7 +4,7 @@ import Logo2 from "../../assets/logo/logo-2.png";
 const Header = ({ darkVersion }) => {
   return (
     <div className="col-12">
-      <header className="py-4 px-4 text-center">
+      <header className="py-2 px-2 text-center">
         <img
           src={darkVersion ? Logo2 : Logo}
           className="img-fluid"
--- web/src/index.js
+++ web/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./styles/global.css";
 import Cadastro from "./pages/cadastro/index";
+import Checkout from "./pages/checkout/index";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Cadastro />
+    {/* <Cadastro /> */}
+    <Checkout />
   </React.StrictMode>,
   document.getElementById("root")
 );
