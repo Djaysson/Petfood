dd994470ff2272d241531b4283d574f79ff8107c
Djaysson <djayson.dev@gmail.com>
2021-11-26 23:14:09 -0300

    Feat(petshop): add to petshop product catalog page

--- web/src/index.js
+++ web/src/index.js
@@ -4,11 +4,13 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./styles/global.css";
 import Cadastro from "./pages/cadastro/index";
 import Checkout from "./pages/checkout/index";
+import Petshop from "./pages/petshop/index";
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <Cadastro /> */}
-    <Checkout />
+    {/* <Checkout /> */}
+    <Petshop />
   </React.StrictMode>,
   document.getElementById("root")
 );
